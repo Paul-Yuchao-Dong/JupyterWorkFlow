37a69a12a7ea28bf9d8ebdc2f8a9a2a4e442252c
Paul-Yuchao-Dong <paulcynic@gmail.com>
2017-03-04 23:02:15 +0800

    Move data download to jupyter work flow package

---
+++ jupyterworkflow/data.py
@@ -0,0 +1,29 @@
+import os
+from urllib.request import urlretrieve
+import pandas as pd
+import matplotlib.pyplot as plt
+
+URL = 'https://data.seattle.gov/api/views/65db-xm6k/rows.csv?accessType=DOWNLOAD'
+
+def get_fremont_data(filename = "Fremont.csv", url = URL, force_download = False):
+    """
+    Download and cache fremont date
+    Parameters
+    -----------
+    filename: string (optional)
+        location to save the data
+    url: string (optional)
+        web location of the data
+    force_download: bool (optional)
+        if True force download the data
+    Returns
+    -----------
+    data: pd.DataFrame
+        the Fremont bridge bike data
+    """
+    if force_download or not os.path.exists(filename):
+        urlretrieve(url, filename)
+    data = pd.read_csv(filename, index_col="Date", parse_dates=True)
+    data.columns = ['West', 'East']
+    data['Total'] = data.sum(axis = 1)
+    return data
