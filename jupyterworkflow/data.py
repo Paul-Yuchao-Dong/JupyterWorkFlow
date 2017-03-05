af590599cbbb6d99649f2ea6ef7c02877468e883
Paul-Yuchao-Dong <paulcynic@gmail.com>
2017-03-05 15:25:18 +0800

    Add final analysis

--- jupyterworkflow/data.py
+++ jupyterworkflow/data.py
@@ -1,13 +1,13 @@
 import os
 from urllib.request import urlretrieve
 import pandas as pd
-import matplotlib.pyplot as plt
 
 URL = 'https://data.seattle.gov/api/views/65db-xm6k/rows.csv?accessType=DOWNLOAD'
 
-def get_fremont_data(filename = "Fremont.csv", url = URL, force_download = False):
-    """
-    Download and cache fremont date
+def get_fremont_data(filename="Fremont.csv",\
+    url = URL, 
+    force_download = False):
+    """Download and cache fremont date
     Parameters
     -----------
     filename: string (optional)
@@ -19,13 +19,12 @@ def get_fremont_data(filename = "Fremont.csv", url = URL, force_download = False
     Returns
     -----------
     data: pd.DataFrame
-        the Fremont bridge bike data
-    """
+        the Fremont bridge bike data"""
     if force_download or not os.path.exists(filename):
         urlretrieve(url, filename)
 
     data = pd.read_csv(filename, index_col="Date")
-    
+
     try:
         data.index = pd.to_datetime(data.index, format="%m/%d/%Y %I:%M:%S %p")
     except ValueError:
